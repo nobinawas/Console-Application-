a6a69cb105aecc6ba5f06462b58731c3641f9b27
nawas <nawasnobi806@gmail.com>
2021-08-17 23:37:20 +0600

    added Hello World in cSharp

---
+++ HelloWorld.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            //First Hello World Typing in Console Application. Before start the co
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+            
+
+
+            // C Sharp Operation Varriable Base 
+
+            /*int x = 100;
+            int y = 50;
+            Console.WriteLine(x + y);
+            Console.ReadLine();
+            */
+
+            // C Sharp Operation Varriable Base 
+            /* int x = 100 + 5;
+             Console.WriteLine(x);
+             Console.ReadLine();
+            */
+
+            /* int sum1 = 100 + 50;
+            int sum2 = sum1 + 250;
+            int sum3 = sum2 + sum2;
+            Console.WriteLine(sum1);
+            Console.WriteLine(sum2);
+            Console.WriteLine(sum3);
+            Console.ReadLine();
+            */
+
+        }
+    }
+}
